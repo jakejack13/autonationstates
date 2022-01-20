d3ed2045411d3fd64988c943a194138ad614d118
Jacob Kerr <jck268@cornell.edu>
2022-01-19 20:00:44 -0500

    Added dispatch functionality, created event loops, and added documentation

--- autonationstates/api.py
+++ autonationstates/api.py
@@ -17,11 +17,13 @@ class NationStates:
     _pin: str
 
     def __init__(self, nation: str, password: str) -> None:
+        """Creates a new NationStates API middleware with nation `nation` and password `password`"""
         self.nation = nation
         self._first_auth(password)
 
 
     def _create_shard_response(self, shard: str) -> requests.Response:
+        """Creates a new shard response for shard `shard`"""
         params = {'nation': self.nation, 'q': shard}
         headers = {'X-Pin': self.pin, 'User-Agent': USERAGENT}
         r = requests.get(BASEURL, params=params, headers=headers)
@@ -31,7 +33,9 @@ class NationStates:
         return r
     
 
-    def _first_auth(self, password):
+    def _first_auth(self, password: str):
+        """Uses the given password `password` to authenticate with NS and
+        generate an autologin and pin"""
         params = {'nation': self.nation, 'q': 'ping'}
         headers = {'X-Password': password, 'User-Agent': USERAGENT}
         r = requests.get(BASEURL, params=params, headers=headers)
@@ -40,6 +44,7 @@ class NationStates:
         
 
     def ping(self) -> None:
+        """Pings NS and refreshes the pin"""
         params = {'nation': self.nation, 'q': 'ping'}
         headers = {'X-Autologin': self.autologin, 'User-Agent': USERAGENT}
         r = requests.get(BASEURL, params=params, headers=headers)
@@ -49,6 +54,7 @@ class NationStates:
 
 
     def get_issues(self) -> dict[str, int]:
+        """Returns the issue id/number of options pairs for the given account"""
         r = self._create_shard_response('issues')
         soup = BeautifulSoup(r.text, 'xml')
         issues: dict[str, int]= {}
@@ -57,8 +63,56 @@ class NationStates:
             issues[tag['id']] = len(tag.find_all('OPTION'))
         return issues
 
-    def answer_issue(self, issue_id, choice) -> bool:
+
+    def issue_info(self, issue_id: str) -> Tag | None:
+        """Returns the parsed XML info of the issue with issue id `issue_id` or
+        `None` if this issue is not found"""
+        r = self._create_shard_response('issues')
+        soup = BeautifulSoup(r.text, 'xml')
+        for tag in soup.find_all('ISSUE'):
+            tag: Tag
+            if tag['id'] == issue_id:
+                return tag
+        return None
+
+
+    def answer_issue(self, issue_id: str, choice: int) -> bool:
+        """Answers the issue of id `issue_id` with choice number `choice` and
+        returns `True` if successful"""
         params = {'nation': self.nation, 'c': 'issue', 'issue': issue_id, 'option': choice}
         headers = {'X-Pin': self.pin, 'User-Agent': USERAGENT}
+        r = requests.get(BASEURL, params=params, headers=headers)
+        if r.status_code == REAUTHCODE:
+            self.ping()
+            return self.answer_issue(issue_id, choice)
+        return r.status_code == 200
+
+
+    def get_next_issue_time(self) -> int:
+        """Returns the time until the next issue in seconds from the epoch"""
+        r = self._create_shard_response('nextissuetime')
+        soup = BeautifulSoup(r.text, 'xml')
+        return int(soup.find('NEXTISSUETIME').text)
+
+
+    def write_dispatch(self, title: str, text: str, category: int = 1, subcategory: int = 100) -> bool:
+        """Writes a dispatch with title `title`, text `text`, category
+        `category`, and subcategory `subcategory` and returns `True` if successful"""
+        params = {'nation': self.nation, 'c': 'dispatch', 'dispatch': 'add',
+            'title': title, 'text': text, 'category': category, 
+            'subcategory': subcategory, 'mode': 'prepare'
+        }
+        headers = {'X-Pin': self.pin, 'User-Agent': USERAGENT}
+        r = requests.get(BASEURL, params=params, headers=headers)
+        if r.status_code == REAUTHCODE:
+            self.ping()
+            return self.write_dispatch(self, title, text, category, subcategory)
+        soup = BeautifulSoup(r.text, 'xml')
+        token = soup.find('SUCCESS').text
+
+        params = {'nation': self.nation, 'c': 'dispatch', 'dispatch': 'add',
+            'title': title, 'text': text, 'category': category, 
+            'subcategory': subcategory, 'mode': 'execute', 'token': token
+        }
         r = requests.get(BASEURL, params=params, headers=headers)
         return r.status_code == 200
--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 import os
+import time
+import asyncio
+import random
+
+import pause
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-
-from autonationstates.api import NationStates
-
 load_dotenv()
 
+from autonationstates import NationStates
+
+
 ns = NationStates(os.getenv('NATION'), os.getenv('PASSWORD'))
-print(ns.get_issues())
+
+def answer_all_issues():
+    """Gets all available issues in the nation, randomly choose an option,
+    write a dispatch about said option, and select choice"""
+    issues = ns.get_issues()
+    for issue in issues:
+        choice = random.randint(0,issues[issue]-1)
+        issueinfo = ns.issue_info(issue)
+        if issueinfo is None:
+            continue
+        title = f"Issue {issue}: {issueinfo.find('TITLE').text}"
+        text = f"""{issueinfo.find('TEXT').text}
+        
+        {ns.nation} answered with choice {choice+1}:
+        {list(issueinfo.find_all('OPTION'))[choice]}"""
+        ns.answer_issue(issue, choice)
+        ns.write_dispatch(title, text)
+
+
+async def issue_loop():
+    """Waits for next issue then answers issues"""
+    while True:
+        answer_all_issues()
+        nextissue = ns.get_next_issue_time()
+        pause.until(nextissue)
+
+asyncio.run(issue_loop())
